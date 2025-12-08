962def4ae20146a56abab6f97c3ae9a9eb741754
Unshakhan <unshak08@gmail.com>
2025-12-08 16:13:44 +0500

    update

--- app.js
+++ app.js
@@ -17,3 +17,74 @@ function underline(){
         var mainText = document.getElementById("text");
         mainText.style.fontFamily = font;
       }
+ function justify(direction) {
+        var mainText = document.getElementById("text");
+        mainText.style.display = "flex";
+        mainText.style.justifyContent = direction;
+      }
+    
+    // ===========================
+    // color apply
+// document.querySelectorAll('.color-box').forEach(box => {
+//   box.addEventListener('click', function () {
+//     const pickedColor = this.getAttribute("data-color");
+//     document.getElementById("text").style.color = pickedColor;
+//   });
+// });
+  var mainText = document.getElementById("text")  
+
+function left(){
+  // mainText.style.display ="flex"
+  mainText.style.textAlign="left"
+}
+function center(){
+  // mainText.style.display ="flex" 
+  mainText.style.textAlign="center"
+}
+function right(){
+  // mainText.style.display ="flex"
+  mainText.style.textAlign="right"
+}
+function changeColor(){
+  var Color = event.target.getAttribute("data-color");
+   document.getElementById("text").style.color = Color;
+}
+
+function changeBgColor(){
+  var BgColor = event.target.getAttribute("data-color");
+document.getElementById("text").style.backgroundColor = BgColor;
+}
+
+
+function makeOrdList() {
+    var editor = document.getElementById("text");
+    editor.focus();
+    document.execCommand("insertOrderedList");
+}
+function makeUnList() {
+    var editor = document.getElementById("text");
+    editor.focus();
+    document.execCommand("insertUnorderedList");
+
+}
+let currentIndent = 0; 
+function indentLeft(){
+ var editor = document.getElementById("text");
+    editor.focus();
+     currentIndent += 10;
+    editor.style.marginLeft = currentIndent + "px";
+}
+function indentright(){
+ var editor = document.getElementById("text");
+    editor.focus();
+    currentIndent -= 10;
+    editor.style.marginLeft = currentIndent + "px";
+}
+function bqoute() {
+    var editor = document.getElementById("text");
+    editor.focus();
+    document.execCommand("blockquote");
+}
+
+
+
